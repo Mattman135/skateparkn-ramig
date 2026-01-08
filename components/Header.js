1e53db8999acecc22b1a8253a7d72ebbe87f791e
Mattis Hallberg <skatemattis97@hotmail.com>
2026-01-08 07:54:12 +0100

    deleted useSearchParams in header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react"
 import { useSearchParams } from "next/navigation"
 import Link from "next/link"
 import Image from "next/image"
-import ButtonSignin from "./ButtonSignin"
 import logo from "@/app/icon.png"
 import config from "@/config"
 
@@ -18,13 +17,7 @@ const links = [
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
 const Header = () => {
-  const searchParams = useSearchParams()
   const [isOpen, setIsOpen] = useState(false)
-
-  // setIsOpen(false) when the route changes (i.e: when the user clicks on a link on mobile)
-  useEffect(() => {
-    setIsOpen(false)
-  }, [searchParams])
 
   return (
     <header className="bg-base-200">
